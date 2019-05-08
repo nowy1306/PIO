58d8ff9419c369d49e7af702eaafc425bf3e2b4a
nowy1306 <49371111+nowy1306@users.noreply.github.com>
2019-05-09 00:06:00 +0200

    Poprawa metody odbicia piłki i generowania klocków.

---
+++ Arkanoid/src/arkanoid/ArkanoidV2/src/arkanoid/ArkanoidMain.java
@@ -0,0 +1,14 @@
+package arkanoid;
+
+public class ArkanoidMain
+{
+
+    public static void main(String[] args)
+    {
+        ArkanoidFrame frame = new ArkanoidFrame("Arkanoid");
+        Menu menu = new Menu();
+        frame.add(menu);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
---
+++ Arkanoid/src/arkanoid/ArkanoidV2/src/arkanoid/MapGenerator.java
@@ -0,0 +1,25 @@
+package arkanoid;
+
+public class MapGenerator
+{
+    public static final int ROW_BRICKS_NUMBER = 7;
+    public static final int COL_BRICKS_NUMBER = 14;
+    public static final double SPACE = 5;
+
+    private MapGenerator(){};
+
+    static Brick[][] generateBricks()
+    {
+        Brick[][] bricks = new Brick[ROW_BRICKS_NUMBER][COL_BRICKS_NUMBER];
+
+        for(int i = 0; i < ROW_BRICKS_NUMBER; i++)
+        {
+            for (int j = 0; j < COL_BRICKS_NUMBER; j++)
+            {
+                bricks[i][j] = new Brick(SPACE + (SPACE + Brick.BRICK_WIDTH) * j, SPACE + (SPACE + Brick.BRICK_HEIGHT) * i, 1);
+            }
+        }
+
+        return bricks;
+    }
+}
